be3d302ba690ccd0a4cd69e1b1d640ae4c178ed4
Takuto Wada <takuto.wada@gmail.com>
2017-02-28 00:51:23 +0900

    refactor: seeking for better names and structure

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var JAPANESE_NUMERAL_CHARS = {
   '九': 9
 };
 
-var EXPONENTS = {
+var EXPONENTS_IN_SUBSEQ = {
   '十': 1,
   '百': 2,
   '千': 3
@@ -25,36 +25,41 @@ var MYRIADS = {
   '兆': 12
 };
 
-function lastValueOf (prev) {
-  if (EXPONENTS[prev.char]) {
-    return prev.value + (1 * Math.pow(10, EXPONENTS[prev.char]));
+function subTotal (acc) {
+  if (EXPONENTS_IN_SUBSEQ[acc.char]) {
+    // if seq starts with 十,百,千, treat them as 一十,一百,一千
+    return acc.value + (1 * Math.pow(10, EXPONENTS_IN_SUBSEQ[acc.char]));
   } else {
-    return prev.value;
+    return acc.value;
   }
 }
 
-function charsToNumber (chars) {
-  return lastValueOf(chars.reduce(function (prev, char) {
-    if (EXPONENTS[char]) {
-      return {value: lastValueOf(prev), exp: EXPONENTS[char], char: char};
+function subseqToNumber (subseq) {
+  return subTotal(subseq.reverse().reduce(function (prev, char) {
+    if (EXPONENTS_IN_SUBSEQ[char]) {
+      return {value: subTotal(prev), exp: EXPONENTS_IN_SUBSEQ[char], char: char};
     }
-    var num = JAPANESE_NUMERAL_CHARS[char];
-    var sum = prev.value + (num * Math.pow(10, prev.exp));
-    return {value: sum, exp: prev.exp + 1, char: char};
+    return {
+      value: prev.value + (JAPANESE_NUMERAL_CHARS[char] * Math.pow(10, prev.exp)),
+      exp: prev.exp + 1,
+      char: char
+    };
   }, {value: 0, exp: 0, char: null}));
 }
 
-function sumOf (acc) {
-  return acc.value + (charsToNumber(acc.seq) * Math.pow(10, acc.exp));
+function sumUp (acc) {
+  return acc.value + (subseqToNumber(acc.seq) * Math.pow(10, acc.exp));
+}
+
+function charsToNumber (chars) {
+  return sumUp(chars.reverse().reduce(function (prev, char) {
+    if (MYRIADS[char]) {
+      return {value: sumUp(prev), exp: MYRIADS[char], seq: []};
+    }
+    return {value: prev.value, exp: prev.exp, seq: [char].concat(prev.seq)};
+  }, {value: 0, exp: 0, seq: []}));
 }
 
 module.exports = function japaneseNumeralsToNumber (stringOfJapaneseNumerals) {
-  var chars = stringOfJapaneseNumerals.split('').reverse();
-  return sumOf(chars.reduce(function (prev, char) {
-    if (MYRIADS[char]) {
-      return {value: sumOf(prev), exp: MYRIADS[char], seq: []};
-    } else {
-      return {value: prev.value, exp: prev.exp, seq: prev.seq.concat([char])};
-    }
-  }, {value: 0, exp: 0, seq: []}));
+  return charsToNumber(stringOfJapaneseNumerals.split(''));
 };
