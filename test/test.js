3cc3eb429fe2f752b3e26c35e0a2d20bebe5c06e
Takuto Wada <takuto.wada@gmail.com>
2017-03-01 22:08:09 +0900

    refactor: reorganize tests

--- test/test.js
+++ test/test.js
@@ -13,30 +13,26 @@ function testConv (spec) {
 }
 
 describe('japanese-numerals-to-number', function () {
-  describe('one digit', function () {
-    testConv({ input: '一', expected: 1 });
-    testConv({ input: '二', expected: 2 });
-  });
-  describe('two or more digits', function () {
+  describe('positional notation', function () {
     testConv({ input: '三〇', expected: 30 });
     testConv({ input: '六八', expected: 68 });
     testConv({ input: '一六〇〇', expected: 1600 });
     testConv({ input: '二〇一七', expected: 2017 });
   });
-  describe('十,百,千', function () {
+  describe('numerals for places: 十,百,千', function () {
     testConv({ input: '二十', expected: 20 });
     testConv({ input: '五十九', expected: 59 });
     testConv({ input: '三百二', expected: 302 });
     testConv({ input: '四千五百', expected: 4500 });
   });
-  describe('if numerals starts with 十,百,千, treat them as 一十,一百,一千', function () {
+  describe('if starts with 十,百,千, treat them as 一十,一百,一千', function () {
     testConv({ input: '十五', expected: 15 });
     testConv({ input: '百八', expected: 108 });
     testConv({ input: '千六', expected: 1006 });
     testConv({ input: '千十', expected: 1010 });
     testConv({ input: '千百十', expected: 1110 });
   });
-  describe('万,億,兆', function () {
+  describe('numerals for myriads: 万,億,兆', function () {
     testConv({ input: '百万', expected: 1000000 });
     testConv({ input: '一万五千八百', expected: 15800 });
     testConv({ input: '二億五千万', expected: 250000000 });
